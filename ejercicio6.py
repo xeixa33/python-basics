fa1fb9597def6d11f74209fb6c09e4f01fcac56d
xeixa33 <jmborg2020@gmail.com>
2026-04-01 11:48:25 +0200

    Update ejercicio6.py
    depuración y mejora

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,7 +1,10 @@
 edad = int(input("¿Cuántos años tienes? "))
-if edad < 18:
-    print("Aun te quedan muchas cosas por vivir y aprender")
-elif 18 <= edad < 33:
+
+if edad < 0:
+    print("¿Me estás vacilando?")
+elif edad < 18:
+    print("Aún te quedan muchas cosas por vivir y aprender")
+elif edad < 33:
     print("Estás en la flor de la vida, aprovecha cada momento y disfruta")
-elif edad >= 33:
-    print("Si estuviésemos en Lumière, ya habrias fallecido...\npor suerte para ti no es el caso ;)")
+else:
+    print("Si estuviésemos en Lumière, ya habrías fallecido...\nPor suerte para ti no es el caso ;)")
