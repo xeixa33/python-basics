6aacf03244824ad929c0d53accff6b131d98ba77
xeixa33 <jmborg2020@gmail.com>
2026-04-01 11:09:54 +0200

    Update ejercicio4.py
    añadir un espacio en los prompts: 👉 así cuando el usuario escribe queda más limpio

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,4 +1,4 @@
-nombre = input("Escribe tu nombre:") 
-edad = input("Escribe tu edad:") 
-ciudad = input("Escribe tu ciudad:") 
+nombre = input("Escribe tu nombre: ") 
+edad = input("Escribe tu edad: ") 
+ciudad = input("Escribe tu ciudad: ") 
 print(f"Te llamas {nombre}, tienes {edad} años y vives en {ciudad}")
