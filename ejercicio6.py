cc43de633d2b985bb5831bc6fb4e429dadf25dfa
xeixa33 <jmborg2020@gmail.com>
2026-04-01 11:46:38 +0200

    Add files via upload
    ejercicio 6: if y lógica (mayoría de edad)

---
+++ ejercicio6.py
@@ -0,0 +1,7 @@
+edad = int(input("¿Cuántos años tienes? "))
+if edad < 18:
+    print("Aun te quedan muchas cosas por vivir y aprender")
+elif 18 <= edad < 33:
+    print("Estás en la flor de la vida, aprovecha cada momento y disfruta")
+elif edad >= 33:
+    print("Si estuviésemos en Lumière, ya habrias fallecido...\npor suerte para ti no es el caso ;)")
