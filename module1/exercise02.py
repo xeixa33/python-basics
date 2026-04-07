fb5d079dec8fc5e471726c29a3b1847d516016ed
xeixa33 <jmborg2020@gmail.com>
2026-04-08 01:11:37 +0200

    style: change language

---
+++ module1/exercise02.py
@@ -0,0 +1,10 @@
+num1 = 33
+num2 = 66
+
+total = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+print("Sum:", total)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
