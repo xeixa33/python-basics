aadccd1f2e0e71943c2e5b75e5cbce2210e5562b
xeixa33 <jmborg2020@gmail.com>
2026-04-08 01:12:41 +0200

    style: change language

--- module1/ejercicio3.py
+++
@@ -1,6 +0,0 @@
-num = 33
-
-if num % 2 == 0:
-    print("El número es par")
-else:
-    print("El número es impar")
---
+++ module1/exercise03.py
@@ -0,0 +1,6 @@
+num = 33
+
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
