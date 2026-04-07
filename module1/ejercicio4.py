cd562619902f1cd30bff652dbcb1e2adaa66a151
xeixa33 <jmborg2020@gmail.com>
2026-04-08 01:13:32 +0200

    style: change language

--- module1/ejercicio4.py
+++
@@ -1,4 +0,0 @@
-nombre = input("Escribe tu nombre: ") 
-edad = input("Escribe tu edad: ") 
-ciudad = input("Escribe tu ciudad: ") 
-print(f"Te llamas {nombre}, tienes {edad} años y vives en {ciudad}")
---
+++ module1/exercise04.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print(f"Your name is {name}, you are {age} years old and you live in {city}")
